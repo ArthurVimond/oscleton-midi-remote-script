bef72dd2bb8e957316d3f1815b5b8c8d22d41628
Arthur Vimond <arthurvimond@gmail.com>
2020-02-08 16:09:11 +0100

    chore(Oscleton): change midi remote script version to 0.4.0

--- Oscleton.py
+++ Oscleton.py
@@ -13,7 +13,7 @@ from OscletonOSC import OscletonOSC
 class Oscleton(ControlSurface):
 
     # MIDI Remote Script version
-    midi_remote_script_version = '0.3.0'
+    midi_remote_script_version = '0.4.0'
 
 
     def __init__(self, c_instance):
