39399f96f964ad71e3612ae9acf514bfc4acc64d
Arthur Vimond <arthurvimond@gmail.com>
2020-12-19 00:58:02 +0100

    fix(BrowserComponent): fix BrowserItem search for Plugins

--- OscletonBrowserComponent.py
+++ OscletonBrowserComponent.py
@@ -17,16 +17,39 @@ class OscletonBrowserComponent(SessionComponent, OscletonMixin):
         self.add_callback('/live/browser/stop_preview', self._stop_preview)
 
 
-    def _get_browser_item(self, msg, src, useVST2CustomFolder):
+    def _get_browser_item(self, msg, src, useVST2CustomFolder, useAudioUnitsOnly, useVST2Only, useVST3Only):
         browser_item_uri = msg[2]
 
         no_query_uri = self._remove_prefix(browser_item_uri, 'query:')
         decoded_uri = no_query_uri
 
-        # NB: For Plugins/VST folder, if custom folder is not used,
-        # the 'Local' folder is hidden, so it needs to be removed from the uri
-        if not useVST2CustomFolder:
-            decoded_uri = decoded_uri.replace('Plugins#VST:Local', 'Plugins#VST')
+        # NB: When only Audio Units are used,
+        # the 'Audio Units' folder is hidden, so it needs to be removed from the uri
+        if useAudioUnitsOnly:
+            decoded_uri = decoded_uri.replace('Plugins#Audio%20Units', 'Plugins')
+
+        # NB: When only VST2 are used,
+        # the 'VST2' folder is hidden, so it needs to be removed from the uri
+        if useVST2Only:
+            # NB: For Plugins/VST folder, if custom folder is not used,
+            # the 'Local' folder is hidden, so it needs to be removed from the uri
+            if not useVST2CustomFolder:
+                decoded_uri = decoded_uri.replace('Plugins#VST:Local', 'Plugins')
+            else:
+                if 'Plugins#VST:Custom' in decoded_uri:
+                    decoded_uri = decoded_uri.replace('Plugins#VST:Custom', 'Plugins#Custom')
+                elif 'Plugins#VST:Local' in decoded_uri:
+                    decoded_uri = decoded_uri.replace('Plugins#VST:Local', 'Plugins#Local')
+        else:
+            if not useVST2CustomFolder:
+                decoded_uri = decoded_uri.replace('Plugins#VST:Local', 'Plugins#VST')
+        
+        # NB: When only VST3 are used,
+        # the 'VST3' folder is hidden, so it needs to be removed from the uri
+        if useVST3Only:
+            decoded_uri = decoded_uri.replace('Plugins#VST3', 'Plugins')
+
+        self.log_message('--> decoded_uri: ' + decoded_uri)
 
         nodes = re.split('[#:]', decoded_uri)
         parent_nodes = nodes[:-1]
@@ -57,11 +80,11 @@ class OscletonBrowserComponent(SessionComponent, OscletonMixin):
             return browser_item
 
         node_index = 1
-        browser_item = self._find_next_node_browser_item(browser_item_uri, browser_item, nodes, node_index, useVST2CustomFolder)
+        browser_item = self._find_next_node_browser_item(browser_item_uri, browser_item, nodes, node_index, useVST2CustomFolder, useAudioUnitsOnly, useVST2Only, useVST3Only)
         return browser_item
 
 
-    def _find_next_node_browser_item(self, browser_item_uri, browser_item, nodes, node_index, useVST2CustomFolder):
+    def _find_next_node_browser_item(self, browser_item_uri, browser_item, nodes, node_index, useVST2CustomFolder, useAudioUnitsOnly, useVST2Only, useVST3Only):
 
         if node_index == len(nodes):
             return browser_item
@@ -77,23 +100,47 @@ class OscletonBrowserComponent(SessionComponent, OscletonMixin):
 
             child_browser_item_no_query_uri = self._remove_prefix(child_browser_item.uri, 'query:')
 
-            # NB: For Plugins/VST folder, if custom folder is not used,
-            # the 'Local' folder is hidden, so it needs to be removed from the uri
-            if not useVST2CustomFolder:
-                child_browser_item_no_query_uri = child_browser_item_no_query_uri.replace('Plugins#VST:Local', 'Plugins#VST')
+            # NB: When only Audio Units is used,
+            # the 'Audio Units' folder is hidden, so it needs to be removed from the uri
+            if useAudioUnitsOnly:
+                child_browser_item_no_query_uri = child_browser_item_no_query_uri.replace('Plugins#Audio%20Units', 'Plugins')
+            
+            # NB: When only VST2 is used,
+            # the 'VST2' folder is hidden, so it needs to be removed from the uri
+            if useVST2Only:
+                # NB: For Plugins/VST folder, if custom folder is not used,
+                # the 'Local' folder is hidden, so it needs to be removed from the uri
+                if not useVST2CustomFolder:
+                    child_browser_item_no_query_uri = child_browser_item_no_query_uri.replace('Plugins#VST:Local', 'Plugins')
+                else:
+                    if 'Plugins#VST:Custom' in child_browser_item_no_query_uri:
+                        child_browser_item_no_query_uri = child_browser_item_no_query_uri.replace('Plugins#VST:Custom', 'Plugins#Custom')
+                    elif 'Plugins#VST:Local' in child_browser_item_no_query_uri:
+                        child_browser_item_no_query_uri = child_browser_item_no_query_uri.replace('Plugins#VST:Local', 'Plugins#Local')
+            else:
+                if not useVST2CustomFolder:
+                    child_browser_item_no_query_uri = child_browser_item_no_query_uri.replace('Plugins#VST:Local', 'Plugins#VST')
+            
+            # NB: When only VST3 is used,
+            # the 'VST3' folder is hidden, so it needs to be removed from the uri
+            if useVST3Only:
+                child_browser_item_no_query_uri = child_browser_item_no_query_uri.replace('Plugins#VST3', 'Plugins')
 
             child_nodes = re.split('[#:]', child_browser_item_no_query_uri)
 
             if child_nodes == sub_nodes:
                 next_node_index = node_index + 1
-                return self._find_next_node_browser_item(browser_item_uri, child_browser_item, nodes, next_node_index, useVST2CustomFolder)
+                return self._find_next_node_browser_item(browser_item_uri, child_browser_item, nodes, next_node_index, useVST2CustomFolder, useAudioUnitsOnly, useVST2Only, useVST3Only)
 
 
     def _preview_browser_item(self, msg, src):
 
         useVST2CustomFolder = self._checkVST2CustomFolderUse()
+        useAudioUnitsOnly = self._checkAudioUnitsOnlyUse()
+        useVST2Only = self._checkVST2OnlyUse()
+        useVST3Only = self._checkVST3OnlyUse()
 
-        browser_item = self._get_browser_item(msg, src, useVST2CustomFolder)
+        browser_item = self._get_browser_item(msg, src, useVST2CustomFolder, useAudioUnitsOnly, useVST2Only, useVST3Only)
         if browser_item is not None:
             self._browser.preview_item(browser_item)
 
@@ -101,8 +148,11 @@ class OscletonBrowserComponent(SessionComponent, OscletonMixin):
     def _load_browser_item(self, msg, src):
 
         useVST2CustomFolder = self._checkVST2CustomFolderUse()
+        useAudioUnitsOnly = self._checkAudioUnitsOnlyUse()
+        useVST2Only = self._checkVST2OnlyUse()
+        useVST3Only = self._checkVST3OnlyUse()
 
-        browser_item = self._get_browser_item(msg, src, useVST2CustomFolder)
+        browser_item = self._get_browser_item(msg, src, useVST2CustomFolder, useAudioUnitsOnly, useVST2Only, useVST3Only)
         if browser_item is not None:
             self._browser.load_item(browser_item)
 
@@ -112,8 +162,11 @@ class OscletonBrowserComponent(SessionComponent, OscletonMixin):
         page_size = msg[4]
 
         useVST2CustomFolder = self._checkVST2CustomFolderUse()
+        useAudioUnitsOnly = self._checkAudioUnitsOnlyUse()
+        useVST2Only = self._checkVST2OnlyUse()
+        useVST3Only = self._checkVST3OnlyUse()
 
-        browser_item = self._get_browser_item(msg, src, useVST2CustomFolder)
+        browser_item = self._get_browser_item(msg, src, useVST2CustomFolder, useAudioUnitsOnly, useVST2Only, useVST3Only)
         if browser_item is not None:
             children_items_json_array = []
             collect_started = False
@@ -151,15 +204,64 @@ class OscletonBrowserComponent(SessionComponent, OscletonMixin):
         }
         browser_item_json = microjson.to_json(browser_item_mjson)
         return str(browser_item_json)
+    
 
     def _checkVST2CustomFolderUse(self):
         plugins_browser_items = self._browser.plugins.children
         for plugin_browser_item in plugins_browser_items:
-            if plugin_browser_item.name == 'VST':
-                for vst_browser_item_child in plugin_browser_item.children:
-                    if vst_browser_item_child.name == 'Custom':
+            if plugin_browser_item.uri == 'query:Plugins#VST':
+                for vst_plugin_browser_item_uri in plugin_browser_item.children:
+                    if vst_plugin_browser_item_uri.uri == 'query:Plugins#VST:Custom':
                         return True
         return False
+    
+
+    def _checkAudioUnitsUse(self):
+        plugins_browser_items = self._browser.plugins.children
+        for plugin_browser_item in plugins_browser_items:
+            if 'query:Plugins#Audio%20Units' in plugin_browser_item.uri:
+                return True
+        
+        return False
+    
+
+    def _checkVST2Use(self):
+        plugins_browser_items = self._browser.plugins.children
+        for plugin_browser_item in plugins_browser_items:
+            if 'query:Plugins#VST:Local' in plugin_browser_item.uri or plugin_browser_item.uri == 'query:Plugins#VST':
+                return True
+        
+        return False
+    
+
+    def _checkVST3Use(self):
+        plugins_browser_items = self._browser.plugins.children
+        for plugin_browser_item in plugins_browser_items:
+            if 'query:Plugins#VST3' in plugin_browser_item.uri:
+                return True
+        
+        return False
+    
+
+    def _checkAudioUnitsOnlyUse(self):
+        useAudioUnits = self._checkAudioUnitsUse()
+        useVST2 = self._checkVST2Use()
+        useVST3 = self._checkVST3Use()
+        return useAudioUnits and not useVST2 and not useVST3
+    
+
+    def _checkVST2OnlyUse(self):
+        useAudioUnits = self._checkAudioUnitsUse()
+        useVST2 = self._checkVST2Use()
+        useVST3 = self._checkVST3Use()
+        return not useAudioUnits and useVST2 and not useVST3
+    
+
+    def _checkVST3OnlyUse(self):
+        useAudioUnits = self._checkAudioUnitsUse()
+        useVST2 = self._checkVST2Use()
+        useVST3 = self._checkVST3Use()
+        return not useAudioUnits and not useVST2 and useVST3
 
 
     def _stop_preview(self, msg, src):
